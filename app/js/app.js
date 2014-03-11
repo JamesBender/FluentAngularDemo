c19b045cf6a22c8db6556df57a72ba48a510c82b
JamesBender <James@JamesCBender>
2014-03-11 16:12:01 -0700

    added during demo

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,15 @@
-window.App = angular.module('app', []);
+window.App = angular.module('app', ['ngRoute']);
+
+App.config(function ($routeProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when('/hello', {
+		templateUrl: "hello.html"
+	})
+	.when('/goodbye', {
+		template: "goodbye"
+	})
+	.otherwise({
+		template: "404!"
+	});
+});
